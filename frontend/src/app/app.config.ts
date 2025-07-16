d6cf34ce255d53327366a8edc93828ceac44c978
Miguelmo <miguelmoyaortega@gmail.com>
2025-07-16 17:36:17 +0200

    feat(functions): initialize Firebase functions with TypeScript setup
    - Added package.json for functions with scripts for linting, building, serving, and deploying.
    - Created index.ts with a sample helloWorld function using Firebase Functions.
    - Configured TypeScript with tsconfig.json and tsconfig.dev.json for development.
    - Updated main project package.json with scripts for running emulators and frontend.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,43 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'jobby-5d1f6',
+        appId: '1:600179747387:web:4749b72ffe4ef949684a1e',
+        storageBucket: 'jobby-5d1f6.firebasestorage.app',
+        apiKey: 'AIzaSyD5mAjql605YO1pB33KlTcA1JEg-ezBbI8',
+        authDomain: 'jobby-5d1f6.firebaseapp.com',
+        messagingSenderId: '600179747387',
+        measurementId: 'G-8RND59PCB4',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideFunctions(() => getFunctions()),
+
+    ScreenTrackingService,
+    UserTrackingService,
+  ],
 };
